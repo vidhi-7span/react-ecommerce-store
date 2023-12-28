c8181b410dd23e0769ef11afc4c0cad5ed9b0c8d
vidhi-7span <vidhi@7span.com>
2023-12-28 12:25:20 +0530

    Create service and trusted section in home page

--- src/About.jsx
+++ src/About.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 
 const About = () => {
-  const data={
-    name:"About Us"
-  }
-  return (
-    <HeroSection myData={data}/>
-  );
+  const data = {
+    name: "About Us",
+  };
+  return <HeroSection myData={data} />;
 };
 
 export default About;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import Trusted from "./components/Trusted";
+import Services from "./components/Services";
+
 const Home = () => {
-  const data={
-    name:"ViDStore"
-  }
-  return <HeroSection myData={data}/>;
+  const data = {
+    name: "ViDStore",
+  };
+  return (
+    <div>
+      <HeroSection myData={data} />
+      <Services/>
+      <Trusted />
+    </div>
+  );
 };
 
 export default Home;
